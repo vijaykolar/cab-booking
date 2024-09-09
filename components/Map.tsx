b80c7cac933927dcd14cf66ddab42c74bb8481f2
Vijay Kolar <vijayikolar@gmail.com>
2024-09-09 22:11:03 +0530

    working on maps

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,10 +1,21 @@
 import { Text, View } from "react-native";
+import MapView, { PROVIDER_DEFAULT } from "react-native-maps";
 
 function Map() {
+  const region = {};
   return (
-    <View>
+    <MapView
+      provider={PROVIDER_DEFAULT}
+      showsMyLocationButton={true}
+      showsUserLocation={true}
+      className="w-full h-full rounded-2xl"
+      tintColor="black"
+      mapType="mutedStandard"
+      showsPointsOfInterest={false}
+      // userInterfaceStyle="light"
+    >
       <Text>Map</Text>
-    </View>
+    </MapView>
   );
 }
 
